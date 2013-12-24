c9f73b159523eab4decc117b740f9a0def85ef14
chuross <azarasi.mo.mo@gmail.com>
2013-12-25 00:58:26 +0900

    Ubuntu用の設定を追加

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,16 @@
-#
-# Cookbook Name:: gui
-# Recipe:: default
-#
-# Copyright 2013, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+if platform?('ubuntu')
+	execute "apt-get-update" do
+		command "apt-get update"
+		ignore_failure true
+		only_if do
+			File.exists?('/var/lib/apt/periodic/update-success-stamp') &&
+			File.mtime('/var/lib/apt/periodic/update-success-stamp') < Time.now - 86400
+		end
+	end
+	
+	package 'ubuntu-desktop' do 
+		action :install
+	end
+else
+	Chef::Application.fatal!('not supported on other OS.')
+end
