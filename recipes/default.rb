5105efc70b487bb54da1a93606e41fd1eef858b3
chuross <azarasi.mo.mo@gmail.com>
2014-01-25 03:51:58 +0900

    Ubuntuのバージョン別の処理を追加した

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,6 @@
 if platform?('ubuntu')
 	execute "apt-get-update" do
 		command "apt-get update"
-		ignore_failure true
-		only_if do
-			File.exists?('/var/lib/apt/periodic/update-success-stamp') &&
-			File.mtime('/var/lib/apt/periodic/update-success-stamp') < Time.now - (60 * 60 * 24)
-		end
 	end
 	
 	package 'ubuntu-desktop' do 
--- recipes/ja.rb
+++ recipes/ja.rb
@@ -1,20 +1,35 @@
-include_recipe 'chef-gui::default'
+include_recipe 'gui::default'
 
 if platform?('ubuntu')
+	list_name = nil
+
+	version = node['platform_version']
+	if version >= '13.10'
+		list_name = 'saucy'
+	elsif version >= '13.04'
+		list_name = 'raring'
+	elsif version >= '12.10'
+		list_name = 'quantal'
+	else
+		list_name = 'precise'
+	end
+
 	bash 'add-repository' do
 		cwd '/tmp'
 		code <<-EOH
 		wget -q https://www.ubuntulinux.jp/ubuntu-ja-archive-keyring.gpg -O- | apt-key add -
-        wget -q https://www.ubuntulinux.jp/ubuntu-jp-ppa-keyring.gpg -O- | apt-key add -
-        wget https://www.ubuntulinux.jp/sources.list.d/raring.list -O /etc/apt/sources.list.d/ubuntu-ja.list
-        apt-get update
-        apt-get -y upgrade
+		wget -q https://www.ubuntulinux.jp/ubuntu-jp-ppa-keyring.gpg -O- | apt-key add -
+		wget https://www.ubuntulinux.jp/sources.list.d/#{list_name}.list -O /etc/apt/sources.list.d/ubuntu-ja.list
+		apt-get update
+		apt-get -y upgrade
 		EOH
 		creates '/etc/apt/sources.list.d/ubuntu-ja.list'
 	end
 
-	package 'ubuntu-defaults-ja' do
-		action :install
+	%w{language-pack-ja language-pack-ja-base language-pack-gnome-ja ubuntu-defaults-ja}.each do |pkg|
+		package pkg do
+			action :install
+		end
 	end
 
 	template '/etc/default/locale' do
